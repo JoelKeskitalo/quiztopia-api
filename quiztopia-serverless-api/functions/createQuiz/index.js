936e5dbc2255b2c7775c1f7d725f0aec86e85be3
J0113boy <keskitalo_6@hotmail.com>
2024-09-30 11:56:26 +0200

    createQuiz with extra controller complete

---
+++ quiztopia-serverless-api/functions/createQuiz/index.js
@@ -0,0 +1,96 @@
+const { dynamoDb } = require('../../database/db')
+const { v4: uuidv4 } = require('uuid')
+
+module.exports.handler = async (event) => {
+    try {
+        const { userId, quizName, questions,  } = JSON.parse(event.body)
+
+        if (!userId || !quizName || !questions) {
+            return {
+                responseCode: 400,
+                body: JSON.stringify({
+                    message: 'Please fill in the required forms'
+                })
+            }
+        }
+
+        if(!Array.isArray(questions) || questions.length === 0) {
+            return {
+                statusCode: 400,
+                body: JSON.stringify({
+                    message: 'Please provide at least one question'
+                })
+            }
+        }
+
+
+        const scanParams = {
+            TableName: 'Quiztopia-Quiz',
+            FilterExpression: 'quizName = :quizName',
+            ExpressionAttributeValues: {
+                ':quizName': quizName
+            }
+        }
+
+        const scanResult = await dynamoDb.scan(scanParams)
+        const existingName = scanResult.Items[0]
+
+        if(existingName) {
+            return {
+                statusCode: 400,
+                body: JSON.stringify({
+                    message: 'That quiz name is already taken'
+                })
+            }
+        }
+
+        const quizId = uuidv4()
+        const createdAt = new Date().toISOString()
+
+
+        const createQuizParams = {
+            TableName: 'Quiztopia-Quiz',
+            Item: {
+                quizId,
+                userId,
+                quizName,
+                questions,
+                createdAt: createdAt
+            }
+        }
+
+        try {
+            await dynamoDb.put(createQuizParams);
+            console.log("Successfully inserted into DynamoDB:", createQuizParams.Item);
+        } catch (error) {
+            console.error("Failed to insert into DynamoDB", error);
+            return {
+                statusCode: 500,
+                body: JSON.stringify({
+                    error: error.message
+                })
+            };
+        }
+        
+
+        console.log(questions)
+
+        return {
+            statusCode: 200,
+            body: JSON.stringify({
+                message: 'Quiz created successfully',
+                quiz: quizId,
+                user: userId
+            })
+        }
+
+
+    } catch(error) {
+        return {
+            statusCode: 500,
+            body: JSON.stringify({
+                error: error.message
+            })
+        }
+    }
+}
